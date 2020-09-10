986a9d5ec4ccd5f144dcdd1f902308dbd2451ee1
ChandaniDev <chandani@cryptoassetrating.com>
2020-09-10 18:15:11 +0530

    changes done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,29 @@
-//script for owl-carousel slider
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  dots: true,
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: true,
+$(document).ready(function () {
+  //script for owl-carousel slider
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
     },
-    600: {
-      items: 2,
-      nav: false,
-    },
-    1000: {
-      items: 3,
-      nav: true,
-      loop: false,
-    },
-  },
-});
-// script for navbar (on scroll)
-$(window).on("scroll", function () {
-  if ($(window).scrollTop()) $("nav").addClass("purple");
-  else $("nav").removeClass("purple");
+  });
+  // script for navbar (on scroll)
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop()) $("nav").addClass("purple");
+    else $("nav").removeClass("purple");
+  });
 });
